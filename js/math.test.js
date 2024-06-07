10620c33410e9a852c429b09d5ee10a0a899b710
Zeke <egonzalezhidalgo@student.fullsail.edu>
2024-06-07 14:52:29 -0400

    Reviewed Comments, assignment ready to be delivered.

--- js/math.test.js
+++ js/math.test.js
@@ -1,33 +1,32 @@
+// Importing the modules
 const {
-    add,
-    diff,
-    prod,
-    div,
-    sqrt,
-    max,
-} = require("./math");
+        sum,
+        diff,
+        prod,
+        div,
+        sqrt,
+        max
+    } = require("./math");
 
-describe("Testing the math modules for the assignment",()=>{
-    test("Should add two numbers", ()=>{
-        expect(add(2,2)).toBe(4)
+// Applying the required tests.
+//Refer to test descriptions for individual explanations.
+describe("Testing the math modules for the assignment", () => {
+    test("Return the sum of two numbers", () => {
+        expect(sum(2, 2)).toBe(4);
     });
-    test("Should subtract two numbers", ()=>{
-        expect(diff(4,2)).toBe(2)
+    test("Return the difference of two numbers", () => {
+        expect(diff(4, 2)).toBe(2);
     });
-    test("Should multiply two numbers", ()=>{
-        expect(prod(4,2)).toBe(8)
+    test("Return the product of two numbers", () => {
+        expect(prod(4, 2)).toBe(8);
     });
-    test("Should return the quotient of the division of two numbers rounded to the second decimal place", ()=>{
-        expect(div(4,2)).toBe(2)
+    test("Return the quotient of two numbers rounded to two decimal places", () => {
+        expect(div(4, 2)).toBe(2);
     });
-    test("Should return the square root of a number rounded to the second decimal place", ()=>{
-        expect(sqrt(100)).toBe(10.00)
+    test("Return the square root of a number rounded to two decimal places", () => {
+        expect(sqrt(100)).toBe(10.00);
     });
-    test("Should return the square root of a number rounded to the second decimal place", ()=>{
-        expect(sqrt(100)).toBe(10.00)
+    test("Return the larger of two numbers", () => {
+        expect(max(100, 99)).toBe(100);
     });
-    test("Should return the square root of a number rounded to the second decimal place", ()=>{
-        expect(max(100, 99)).toBe(100)
-    });
-
 });
