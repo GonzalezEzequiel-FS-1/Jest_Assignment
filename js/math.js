499aa2081ce9356b271fd7209965d96931523b27
Zeke <egonzalezhidalgo@student.fullsail.edu>
2024-06-07 14:37:36 -0400

    Fixed toFixed() working correctly, assignment ready.

--- js/math.js
+++ js/math.js
@@ -1,9 +1,9 @@
-const add=(numOne, numTwo)=> parseInt(numOne+numTwo);
-const diff=(numOne, numTwo)=> parseInt(numOne-numTwo);
-const prod=(numOne, numTwo)=> parseInt(numOne*numTwo);
-const div=(numOne, numTwo)=> parseInt(numOne/numTwo);
-const sqrt = ( num )=> parseInt(Math.sqrt(num));
-const max = (numOne, numTwo) => parseInt(Math.max(numOne , numTwo));
+const add = (numOne, numTwo) => parseFloat(numOne + numTwo);
+const diff = (numOne, numTwo) => parseFloat(numOne - numTwo);
+const prod = (numOne, numTwo) => parseFloat(numOne * numTwo);
+const div = (numOne, numTwo) => parseFloat((numOne / numTwo).toFixed(2));
+const sqrt = (num) => parseInt(Math.sqrt(num).toFixed(2));
+const max = (numOne, numTwo) => parseFloat(Math.max(numOne, numTwo).toFixed(2));
 
 module.exports = {
     add,
@@ -12,4 +12,4 @@ module.exports = {
     div,
     sqrt,
     max
-}
+};
